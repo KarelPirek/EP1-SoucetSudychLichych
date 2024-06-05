e985636b6cd89d808090eb973bd57a0b038abf32
Karel Pírek <Pirek.Karel@stud.vassboskovice.cz>
2024-06-05 11:02:42 +0200

    úkol 5

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -1,7 +1,29 @@
 Imports System
 
 Module Program
-  Sub Main(args As String())
-    Console.WriteLine("Hello World!")
-  End Sub
+    Sub Main(args As String())
+        Dim cislo As Integer
+        Dim pocetopakovani As Integer
+
+
+
+        If pocetopakovani = 1 Then
+            Console.WriteLine($"Zadej {pocetopakovani} celé èíslo.")
+        ElseIf 1 > pocetopakovani < 5 Then
+            Console.WriteLine($"Zadej {pocetopakovani} celá èísla.")
+        Else Console.WriteLine($"Zadej {pocetopakovani} celých èísel.")
+        End If
+
+        Do
+            pocetopakovani = pocetopakovani - 1
+            cislo = Console.ReadLine()
+
+            If pocetopakovani = 0 Then
+                Exit Do
+            End If
+        Loop
+
+
+
+    End Sub
 End Module
