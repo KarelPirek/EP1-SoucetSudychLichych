c2e44ad385c1897caac4807e48e4c760801f9f80
Karel Pírek <Pirek.Karel@stud.vassboskovice.cz>
2024-06-05 11:17:23 +0200

    opravy

--- EP1-SoucetSudychLichych/Program.vb
+++ EP1-SoucetSudychLichych/Program.vb
@@ -5,25 +5,27 @@ Module Program
         Dim cislo As Integer
         Dim pocetopakovani As Integer
 
-
+        Console.WriteLine("Zadej, kolik bude možné zadat èísel.")
         If Integer.TryParse(Console.ReadLine, pocetopakovani) Then
-                If pocetopakovani = 1 Then
-                    Console.WriteLine($"Zadej {pocetopakovani} celé èíslo.")
-                ElseIf 1 > pocetopakovani < 5 Then
-                    Console.WriteLine($"Zadej {pocetopakovani} celá èísla.")
-                Else Console.WriteLine($"Zadej {pocetopakovani} celých èísel.")
-                End If
-                Do
-                    pocetopakovani = pocetopakovani - 1
+            If pocetopakovani = 1 Then
+                Console.WriteLine($"Zadej {pocetopakovani} celé èíslo.")
+            ElseIf 1 < pocetopakovani < 5 Then
+                Console.WriteLine($"Zadej {pocetopakovani} celá èísla.")
+            ElseIf pocetopakovani > 4 Then
+                Console.WriteLine($"Zadej {pocetopakovani} celých èísel.")
+            End If
+            Do
+                pocetopakovani = pocetopakovani - 1
                 If Integer.TryParse(Console.ReadLine, cislo) Then
 
                     If pocetopakovani = 0 Then
                         Exit Do
                     End If
                 Else Console.WriteLine("Zadávej pouze celá èísla")
+                    Exit Do
                 End If
             Loop
-            Else Console.WriteLine("Zadávej pouze celá èísla")
+        Else Console.WriteLine("Zadávej pouze celá èísla")
             End If
 
 
